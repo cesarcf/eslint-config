9d055cd0f0f80dd581a47f958958b2642c0e007c
Cesar <cesar.carbajo.ferrero@gmail.com>
2020-10-30 12:35:43 +0100

    feat: add utils file

---
+++ utils.js
@@ -0,0 +1,15 @@
+function getWpModuleResolver() {
+	const wpConfig = require('./webpack.config')
+	const {
+		resolve: { alias, extensions: wpExtensions }
+	} = wpConfig({ NODE_ENV: 'development' })
+
+	const wpAlias = []
+	for (var key in alias) {
+		if (alias.hasOwnProperty(key)) wpAlias.push([key, alias[key]])
+	}
+
+	return { wpAlias, wpExtensions }
+}
+
+module.exports = { getWpModuleResolver }
