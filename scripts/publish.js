5b82a7eeaf2796f84909d09853edaf57f77c8ee9
Cesar Carbajo <cesarcf@ext.inditex.com>
2024-01-20 22:58:44 +0100

    feat: add path

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-
+const path = require("path");
 /* eslint-disable no-console */
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-
 (async () => {
   /* eslint-disable-next-line global-require */
+  const basePath = path.resolve(path.join(process.cwd()));
   const packageJson = require(path.join(basePath, "package.json"));
 
   console.log(`🛠️ Publishing @cesarcf/eslint-config release ${packageJson.version} ...`);
